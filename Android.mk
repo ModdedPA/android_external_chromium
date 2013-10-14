0e2a8a7bee92084fa1eb1d90e9f21b6ab8ccc9c1
mikecriggs <michael.s.corigliano@gmail.com>
2013-10-14 17:20:44 -0400

    Android.mk: add '-fno-strict-aliasing' to LOCAL_CPPFLAGS
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -467,8 +467,7 @@ $(GEN):
 LOCAL_GENERATED_SOURCES += $(GEN)
 
 LOCAL_CFLAGS := -DHAVE_CONFIG_H -DANDROID -DEXPAT_RELATIVE_PATH -DALLOW_QUOTED_COOKIE_VALUES -DCOMPONENT_BUILD -DGURL_DLL -fno-strict-aliasing
-LOCAL_CPPFLAGS := -Wno-sign-promo -Wno-missing-field-initializers -fvisibility=hidden -fvisibility-inlines-hidden
-
+LOCAL_CPPFLAGS := -Wno-sign-promo -Wno-missing-field-initializers -fvisibility=hidden -fvisibility-inlines-hidden -fno-strict-aliasing
 # Just a few definitions not provided by bionic.
 LOCAL_CFLAGS += -include "android/prefix.h"
 
